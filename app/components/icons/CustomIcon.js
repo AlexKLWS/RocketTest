c93756fbc60ad9de25a1ff683bfc4b230cc5a6d7
Alex Korzh <alexkorzh7@gmail.com>
2019-03-28 11:41:42 +0300

    Set up custom icons for use

---
+++ app/components/icons/CustomIcon.js
@@ -0,0 +1,6 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons'
+import icomoonSelection from '../../../selection.json'
+
+const CustomIcon = createIconSetFromIcoMoon(icomoonSelection)
+
+export default CustomIcon
