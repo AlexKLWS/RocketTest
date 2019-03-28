cd14351c3bd6b02230731fa04292e89f1f5c8ec1
Alex Korzh <alexkorzh7@gmail.com>
2019-03-29 01:17:30 +0300

    Tweak expanded music tile layout

--- app/components/elements/music/MusicTile.js
+++ app/components/elements/music/MusicTile.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
+import { connect } from 'react-redux'
+import { StyleSheet, View, Text, TouchableOpacity, Slider } from 'react-native'
 import InteractableTile from '../InteractableTile'
 import CustomIcon from '../../icons/CustomIcon'
-import { connectionsGreen, connectionsBlue, connectionsOrange } from '../../../utils/colorConstants'
+import { defaultIconColor } from '../../../utils/colorConstants'
 import { MUSIC_TILE_ID } from '../../../utils/elementsIds'
 
+const mapStateToProps = state => ({
+  expandedElementId: state.elementId
+})
+
+@connect(mapStateToProps)
 export default class MusicTile extends Component {
   render() {
+    const isExpanded = this.props.expandedElementId === MUSIC_TILE_ID
     return (
       <InteractableTile
         ref={component => {
@@ -18,13 +25,39 @@ export default class MusicTile extends Component {
       >
         <Text style={styles.songTitle}>All Star</Text>
         <Text style={styles.artist}>Smash Mouth</Text>
-        <View style={styles.controlsContainer}>
-          <CustomIcon name="Forward" size={17} color="rgba(255, 255, 255, 0.4)" style={styles.rewind} />
+        <View style={isExpanded ? styles.controlsContainerExpanded : styles.controlsContainer}>
+          <CustomIcon
+            name="Forward"
+            size={isExpanded ? 26 : 17}
+            color="rgba(255, 255, 255, 0.4)"
+            style={styles.rewind}
+          />
           <TouchableOpacity onPressIn={this.onPressInContainerHandle} onPressOut={this.onPressOutContainerHandle}>
-            <CustomIcon name="Play" size={29} color="#FFF" />
+            <CustomIcon
+              name="Play"
+              size={isExpanded ? 38 : 29}
+              color={defaultIconColor}
+              style={isExpanded ? styles.playExpanded : {}}
+            />
           </TouchableOpacity>
-          <CustomIcon name="Forward" size={17} color="rgba(255, 255, 255, 0.4)" style={styles.forward} />
+          <CustomIcon
+            name="Forward"
+            size={isExpanded ? 26 : 17}
+            color="rgba(255, 255, 255, 0.4)"
+            style={styles.forward}
+          />
         </View>
+        {isExpanded && (
+          <View style={styles.volumeSliderContainer}>
+            <CustomIcon name="Less-Volume" size={17} color="rgba(255, 255, 255, 0.4)" />
+            <Slider
+              minimumTrackTintColor="#FFF"
+              maximumTrackTintColor="rgba(255, 255, 255, 0.2)"
+              style={styles.slider}
+            />
+            <CustomIcon name="Volume" size={22} color="rgba(255, 255, 255, 0.4)" />
+          </View>
+        )}
       </InteractableTile>
     )
   }
@@ -61,14 +94,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingTop: 24
   },
+  controlsContainerExpanded: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 80
+  },
   rewind: {
     transform: [{ rotate: '180deg' }],
     paddingLeft: 24,
     paddingRight: 16
   },
-  play: {},
+  playExpanded: {
+    paddingHorizontal: 40
+  },
   forward: {
     paddingLeft: 20,
     paddingRight: 18
+  },
+  slider: {
+    width: 150,
+    marginHorizontal: 15
+  },
+  volumeSliderContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 85
   }
 })
