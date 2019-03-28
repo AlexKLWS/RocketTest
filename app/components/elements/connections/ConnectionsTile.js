fe8d544e46eae982be0e350eaf84a03c378cfc2d
Alex Korzh <alexkorzh7@gmail.com>
2019-03-28 21:20:00 +0300

    Add expandable menu

--- app/components/elements/connections/ConnectionsTile.js
+++ app/components/elements/connections/ConnectionsTile.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, TouchableWithoutFeedback } from 'react-native'
+import { connect } from 'react-redux'
+import { StyleSheet, View, Text, TouchableWithoutFeedback } from 'react-native'
 import InteractableTile from '../InteractableTile'
 import ConnectionsButton from './ConnectionsButton'
 import { connectionsGreenColor, connectionsBlueColor, connectionsOrangeColor } from '../../../utils/colorConstants'
 import { CONNECTIONS_TILE_ID } from '../../../utils/elementsIds'
 
+const mapStateToProps = state => ({
+  expandedElementId: state.elementId
+})
+
+@connect(mapStateToProps)
 export default class ConnectionsTile extends Component {
   render() {
+    const isExpanded = this.props.expandedElementId === CONNECTIONS_TILE_ID
     return (
       <InteractableTile
         ref={component => {
@@ -16,41 +23,109 @@ export default class ConnectionsTile extends Component {
         rowId={this.props.rowId}
         elementId={CONNECTIONS_TILE_ID}
       >
-        <View style={styles.buttonContainer}>
+        <View style={isExpanded ? styles.expandedButtonContainer : styles.buttonContainer}>
           <View style={styles.buttonRow}>
-            <ConnectionsButton
-              name="Aeroplane-Mode"
-              size={22}
-              style={{ paddingLeft: 3 }}
-              activeColor={connectionsOrangeColor}
-              onPressInContainerHandle={this.onPressInContainerHandle}
-              onPressOutContainerHandle={this.onPressOutContainerHandle}
-            />
-            <ConnectionsButton
-              name="Carrier"
-              size={26}
-              style={{ paddingLeft: 1 }}
-              activeColor={connectionsGreenColor}
-              onPressInContainerHandle={this.onPressInContainerHandle}
-              onPressOutContainerHandle={this.onPressOutContainerHandle}
-            />
+            <View style={isExpanded ? styles.expandedButtonHolder : {}}>
+              <ConnectionsButton
+                name="Aeroplane-Mode"
+                size={22}
+                style={{ paddingLeft: 3 }}
+                activeColor={connectionsOrangeColor}
+                onPressInContainerHandle={this.onPressInContainerHandle}
+                onPressOutContainerHandle={this.onPressOutContainerHandle}
+              />
+              {isExpanded && (
+                <React.Fragment>
+                  <Text style={styles.label}>Airplane Mode</Text>
+                  <Text style={styles.label}>On/Off</Text>
+                </React.Fragment>
+              )}
+            </View>
+            <View style={isExpanded ? styles.expandedButtonHolder : {}}>
+              <ConnectionsButton
+                name="Carrier"
+                size={26}
+                style={{ paddingLeft: 1 }}
+                activeColor={connectionsGreenColor}
+                onPressInContainerHandle={this.onPressInContainerHandle}
+                onPressOutContainerHandle={this.onPressOutContainerHandle}
+              />
+              {isExpanded && (
+                <React.Fragment>
+                  <Text style={styles.label}>Mobile Data</Text>
+                  <Text style={styles.label}>On/Off</Text>
+                </React.Fragment>
+              )}
+            </View>
           </View>
           <View style={styles.buttonRow}>
-            <ConnectionsButton
-              name="Wifi"
-              size={19}
-              activeColor={connectionsBlueColor}
-              onPressInContainerHandle={this.onPressInContainerHandle}
-              onPressOutContainerHandle={this.onPressOutContainerHandle}
-            />
-            <ConnectionsButton
-              name="Bluetooth"
-              size={26}
-              activeColor={connectionsBlueColor}
-              onPressInContainerHandle={this.onPressInContainerHandle}
-              onPressOutContainerHandle={this.onPressOutContainerHandle}
-            />
+            <View style={isExpanded ? styles.expandedButtonHolder : {}}>
+              <ConnectionsButton
+                name="Wifi"
+                size={19}
+                activeColor={connectionsBlueColor}
+                onPressInContainerHandle={this.onPressInContainerHandle}
+                onPressOutContainerHandle={this.onPressOutContainerHandle}
+              />
+              {isExpanded && (
+                <React.Fragment>
+                  <Text style={styles.label}>Wi-Fi</Text>
+                  <Text style={styles.label}>On/Off</Text>
+                </React.Fragment>
+              )}
+            </View>
+            <View style={isExpanded ? styles.expandedButtonHolder : {}}>
+              <ConnectionsButton
+                name="Bluetooth"
+                size={26}
+                activeColor={connectionsBlueColor}
+                onPressInContainerHandle={this.onPressInContainerHandle}
+                onPressOutContainerHandle={this.onPressOutContainerHandle}
+              />
+              {isExpanded && (
+                <React.Fragment>
+                  <Text style={styles.label}>Bluetooth</Text>
+                  <Text style={styles.label}>On/Off</Text>
+                </React.Fragment>
+              )}
+            </View>
           </View>
+          {isExpanded && (
+            <View style={styles.buttonRow}>
+              <View style={styles.expandedButtonHolder}>
+                <ConnectionsButton
+                  name="AirDrop"
+                  size={24}
+                  activeColor={connectionsBlueColor}
+                  style={{ paddingLeft: 1 }}
+                  onPressInContainerHandle={this.onPressInContainerHandle}
+                  onPressOutContainerHandle={this.onPressOutContainerHandle}
+                />
+                <Text style={styles.label}>AirDrop</Text>
+                <Text style={styles.label}>On/Off</Text>
+              </View>
+              <View
+                style={{
+                  paddingVertical: 5,
+                  marginLeft: 27,
+                  marginRight: 15,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  flexDirection: 'column'
+                }}
+              >
+                <ConnectionsButton
+                  name="Hotspot"
+                  size={16}
+                  activeColor={connectionsBlueColor}
+                  onPressInContainerHandle={this.onPressInContainerHandle}
+                  onPressOutContainerHandle={this.onPressOutContainerHandle}
+                />
+                <Text style={styles.label}>Personal Hotspot</Text>
+                <Text style={styles.label}>On/Off</Text>
+              </View>
+            </View>
+          )}
         </View>
       </InteractableTile>
     )
@@ -76,9 +151,26 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     flexDirection: 'column'
   },
+  expandedButtonContainer: {
+    paddingVertical: 15,
+    paddingRight: 2,
+    justifyContent: 'space-between',
+    flexDirection: 'column'
+  },
+  expandedButtonHolder: {
+    paddingVertical: 5,
+    marginHorizontal: 27,
+    justifyContent: 'space-between',
+    flexDirection: 'column'
+  },
   buttonRow: {
     justifyContent: 'space-between',
     flexDirection: 'row',
     backgroundColor: 'rgba(0, 0, 0, 0.0)'
+  },
+  label: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 11
   }
 })
