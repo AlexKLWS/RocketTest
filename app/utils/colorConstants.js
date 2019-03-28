8a2c720c1ad21ecd1aed091af4175e756be7bd8a
Alex Korzh <alexkorzh7@gmail.com>
2019-03-28 11:42:11 +0300

    Add color constants

---
+++ app/utils/colorConstants.js
@@ -0,0 +1,4 @@
+export const connectionsDefault = 'rgba(255, 255, 255, 0.2)'
+export const connectionsGreen = '#4CD964'
+export const connectionsBlue = '#007AFF'
+export const connectionsOrange = '#FF9500'
